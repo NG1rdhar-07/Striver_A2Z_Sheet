6e336cab4829ca95d8c14ffba5988f8868dddfab
Noor <noorgirdhar705@gmail.com>
2025-06-01 22:51:31 +0530

    Correction in problem 17....simplified the logic ./output

--- Striver DSA/Step 1 : Learn the basics /Patterns/ptr17.cpp
+++ Striver DSA/Step 1 : Learn the basics /Patterns/ptr17.cpp
@@ -3,30 +3,31 @@ using namespace std;
 
 void pattern17(int number)
 {
-//     for (int i = 0; i<number; i++)
-//     {
-//         int r = i;
-//         for (int j = 0; j < number-i-1; j++)
-//         {
-//             cout << " ";
-//         }
+    for (int i = 0; i<number; i++)
+    {
+        char prev = 'A';
 
-//         for (int j = number-i-1; j < number ; j++)
-//         {
-//             cout << char('A' + r);
-//             r++;
-//         }
+        for (int j = 0; j < number-i-1; j++)
+        {
+            cout << " ";
+        }
+
+        for (int j = number-i-1; j < number ; j++)
+        {
+            cout << char(prev);
+            prev ++;
+        }
         
 
-//         for (int j =number; j<(number + i); j++)
-//         {
-//            cout << char('A' + r - 2);
-//            r--;
-//         }
+        for (int j =number; j<(number + i); j++)
+        {
+           cout << char(prev - 2);
+           prev --;
+        }
 
-//         cout << "\n";
-//     }
-// }
+        cout << "\n";
+    }
+}
 
 int main() {
     int lines;
