a4dabad4f72f17458d78bceb2ba2719becaaddca
Noor <noorgirdhar705@gmail.com>
2025-06-03 16:03:38 +0530

    Fixed code using GPT :)...little error was there

--- Striver DSA/Step 1 : Learn the basics /Patterns/ptr22.cpp
+++ Striver DSA/Step 1 : Learn the basics /Patterns/ptr22.cpp
@@ -3,95 +3,22 @@ using namespace std;
 
 void pattern22(int number)
 {
-    for (int i = 0; i<(2*number)-1; i++)
+    for (int i = 0; i < (2 * number) - 1; i++)
     {
-        if(i<=number-1)
+        for (int j = 0; j < (2 * number) - 1; j++)
         {
-            int decrements = i;
-            int r = number;
-            int j = 0;
-
-            while(j<(2*number)-2) // taaki j ke bahar na print ho !!
-            {
-                while(j<=(number-1))
-                {
-                    if(decrements > 0)
-                    {
-                        cout << r;
-                        r--;
-                        j++;
-                        decrements--;
-                    }
-
-                    else if(decrements == 0)
-                    {
-                        cout << r+1;
-                        j++;
-                    }
-                }
-
-
-                while(j<(2*number)-i-1)  // jahan tak conStant chalega printed value !!
-                    cout << r+1;
-                    j++;
-
-                for(int x = (2*number)-i-1; x<(2*number)-1; x++) // jahan pe changes aane start honge
-                {
-                    cout << r;
-                    r--;
-                }
-                
-            }
-            cout << "\n";
+            int decrements = min(min(i, j), min((2 * number - 2) - i, (2 * number - 2) - j));
+            int r = number - decrements;
+            cout << r;
         }
-         
-        
-        else
-        {
-            int decrements = (2*number)-i-2;
-            int r = number;
-            int j = 0;
-
-            while(j<(2*number)-2)
-                {
-                    while(j<=(number-1))
-                    {
-                        if(decrements > 0)
-                        {
-                            cout << r;
-                            r--;
-                            j++;
-                            decrements--;
-                        }
-
-                        else if(decrements == 0)
-                        {
-                            cout << r+1;
-                            j++;
-                        }
-                    }
-
-                    while(j<i+1)  // jahan tak conStant chalega printed value !!
-                        cout << r+1;
-                        j++;
-
-                    for(int x = i+1; x<(2*number)-1; x++) // jahan pe changes aane start honge
-                    {
-                        cout << r;
-                        r--;
-                    }
-
-                }
-            }
-
-        }
-    
+        cout << "\n";
     }
+}
 
-
-int main() {
+int main()
+{
     int lines;
-    cout << "Enter Number of lines to print: "; 
+    cout << "Enter Number of lines to print: ";
     cin >> lines;
     pattern22(lines);
     return 0;
